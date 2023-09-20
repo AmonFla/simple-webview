91903267ff93830435198e917bd661376253cea4
Flavio Garrido <ing.flaviogarrido@gmail.com>
2023-09-20 08:07:08 -0300

    commit inicial

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { SafeAreaView } from "react-native";
+import { WebView } from 'react-native-webview';
+
+class MyWeb extends Component {
+  render() {
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <WebView 
+          source={{ uri: 'https://www.google.com/' }} 
+        />
+      </SafeAreaView>
+    );
+  }
+}
+
+
+export default MyWeb;
